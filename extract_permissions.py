646d679ad4609ed558f5ba96f2b916a8d848cdae
kamilgeagea <kamilgeagea8199@gmail.com>
2021-03-02 11:03:53 -0500

    implemented the xmlToCSV function for data extraction

--- extract_permissions.py
+++ extract_permissions.py
@@ -1,13 +1,35 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse("./test.xml")
+import pandas as pd
+import csv
 
-permissions = {}
+permissions_labels = []
 
 with open("./all_permissions.txt") as f:
     for l in f:
         length = len(l)
-        permissions[l[19:length-1]] = 0
+        permissions_labels.append(l[19:length-1])
 
-for elem in tree.iter():
-    if elem.tag == "permission" or elem.tag == "uses-permission":
-        print(elem)
+permissions_label = sorted(permissions_labels)
+
+data = pd.DataFrame(columns=permissions_labels)
+
+
+def xmlToCSV(data, filename):
+    permissions = {}
+    for permission_label in permissions_labels:
+        permissions[permission_label] = 0
+    tree = ET.parse(filename)
+    for elem in tree.iter():
+        if elem.tag == "permission" or elem.tag == "uses-permission":
+            permissions[elem.attrib["{http://schemas.android.com/apk/res/android}name"][19:]] = 1
+
+    df = pd.DataFrame(permissions, index=[0]).sort_index(axis=1)
+    return pd.concat([data, df])
+
+
+files = ["./test.xml", "./test.xml"]
+
+for filename in files:
+    data = xmlToCSV(data, filename)
+
+data.to_csv('Export_dataframe.csv', index=False, header=True)
